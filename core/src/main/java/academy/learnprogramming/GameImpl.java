47085956309940a76853b3a5bdbb79d74283e541
flamingCactus1 <dkykosh@novomind.com>
2026-02-17 11:37:42 +0100

    first version of guess the number game.
    created two interfaces NumberGenerator and Game
    
    implemented them with GameImpl and NumberGeneratorImpl

---
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -0,0 +1,103 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GameImpl implements Game {
+
+    //==constants==
+    private static final Logger logger = LoggerFactory.getLogger(GameImpl.class);
+    //==fields==
+    private NumberGenerator numberGenerator;
+    private int guessCount = 10;
+    private int number;
+    private int guess;
+    private int smallest;
+    private int biggest;
+    private int remainingGuesses;
+    private boolean validNumberRange = true;
+
+    //==Constructors==
+
+
+    public GameImpl(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    //==public methods==
+    @Override
+    public void reset() {
+        smallest = 0;
+        biggest = numberGenerator.getMaxNumber();
+        guess = 0;
+        remainingGuesses = guessCount;
+        number = numberGenerator.next();
+        logger.debug("number is {}", number);
+    }
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public int getGuess() {
+        return guess;
+    }
+
+    @Override
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    @Override
+    public int getSmallest() {
+        return smallest;
+    }
+
+    @Override
+    public int getBiggest() {
+        return biggest;
+    }
+
+    @Override
+    public int getRemainingGuesses() {
+        return remainingGuesses;
+    }
+
+    @Override
+    public void check() {
+        checkValidNumberRange();
+        if (validNumberRange) {
+            if (guess > number) {
+                biggest = guess-1;
+            }
+        }
+
+        if (guess < number) {
+            smallest = guess+1;
+        }
+
+        remainingGuesses--;
+    }
+
+    @Override
+    public boolean isValidNumberRange() {
+        return validNumberRange;
+    }
+
+    @Override
+    public boolean isGameLost() {
+        return guess == number;
+    }
+
+    @Override
+    public boolean isGameWon() {
+        return !isGameWon() && remainingGuesses <= 0;
+    }
+
+    //==private methods==
+    private void checkValidNumberRange() {
+        validNumberRange = (guess >= smallest && guess <= biggest);
+    }
+}
--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,12 +2,38 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
+
 
     public static void main(String[] args) {
         logger.info("Guess the number game");
+
+//        create context
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+//        get number generator bean from context (container)
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+//        call method next() to get a random number
+        int number = numberGenerator.next();
+
+        logger.info("received number is {}", number);
+
+//        get game bean from context (container)
+        Game game = context.getBean("game", Game.class);
+
+//        reset game
+        game.reset();
+
+//        close context
+        context.close();
+
+
+
     }
 }
